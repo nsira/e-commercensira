031e93b20805f68b939cc44971fa1699b91077c7
nsira sami <nsirasami45@gmail.com>
2021-09-30 23:37:55 +0100

    2éme commit

--- angular/e-commerce/src/app/app.module.ts
+++ angular/e-commerce/src/app/app.module.ts
@@ -1,18 +1,33 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import{RouterModule,Routes} from '@angular/router';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
-
+import { from } from 'rxjs';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { HomeComponent } from './home/home.component';
+const routes: Routes=[
+  {path:'home',component:HomeComponent},
+ { path:'about', component:AboutComponent},
+ {path:'contact',component:ContactComponent},
+{path:'',redirectTo:'home',pathMatch:'full' },
+  { path: 'route-cart', loadChildren: () => import('./shopping-cart/shopping-cart.module').then(m => m.ShoppingCartModule) }
+];
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
-  ],
+    NavbarComponent,
+    AboutComponent,
+    ContactComponent,
+    HomeComponent  ],
+
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent
+  ]
 })
 export class AppModule { }
